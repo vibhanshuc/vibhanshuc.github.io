1ad41f8ea042d7579d7bd2d42dc5e51df2200ea2
Vibhanshu Chaturvedi <vibhanshu86@gmail.com>
2017-06-09 13:00:30 +0530

    add ga

--- D3/scripts/app.js
+++ D3/scripts/app.js
@@ -1,42 +1,62 @@
 /** author: vibhanshu chaturvedi **/
-'use strict';
-var w = 200, h = 220, padding = 2, dataset = [5, 10, 13, 19, 21, 25, 11, 25, 22, 18 ];
+(function () {
+  'use strict';
+  /* global d3 */
 
-var svg = d3.select('#svg-dataset')
-            .append('svg')
-            .attr({'width': w, 'height': h});
+  var w = 200,
+      h = 220,
+      padding = 2,
+      dataset = [5, 10, 13, 19, 21, 25, 11, 25, 22, 18 ];
 
-svg.selectAll('rect')
+  d3.select('#svg-rect')
+    .append('svg')
+    .append('rect')
+    .attr('width', 200)
+    .attr('height', 300)
+    .style('fill', 'violet');
+
+  d3.select('#svg-circle')
+    .append('svg')
+    .attr('height', '50')
+    .attr('width', '150')
+    .append('circle')
+    .attrs({'cx': 25, 'cy': 25, 'r': 25});
+
+  var svg = d3.select('#svg-dataset')
+    .append('svg')
+    .attrs({'width': w, 'height': h});
+
+  svg.selectAll('rect')
     .data(dataset)
     .enter()
     .append('rect')
-    .attr({
-        x: function (d, i) { return i * (w / dataset.length); },
-        y: function (d) { return h - (d * 4); },
-        width: w / dataset.length - padding,
-        height: function (d) { return d * 4; },
-        fill:  function (d) { return 'rgb(0 ,' + (d * 4 + 10) + ' , 0)'; }
+    .attrs({
+      x: function (d, i) { return i * (w / dataset.length); },
+      y: function (d) { return h - (d * 4); },
+      width: w / dataset.length - padding,
+      height: function (d) { return d * 4; },
+      fill:  function (d) { return 'rgb(0 ,' + (d * 4 + 10) + ' , 0)'; }
     });
 
-svg.selectAll('text')
+  svg.selectAll('text')
     .data(dataset)
     .enter()
     .append('text')
     .text(function (d) { return d; })
-    .attr({
-        'text-anchor': 'middle',
-        x: function (d, i) { return i * (w / dataset.length) + (w / dataset.length - padding) / 2; },
-        y: function (d) { return h - (d * 4) + 14; },
-        'font-size': 12,
-        'fill': '#ffffff'
+    .attrs({
+      'text-anchor': 'middle',
+      x: function (d, i) { return i * (w / dataset.length) + (w / dataset.length - padding) / 2; },
+      y: function (d) { return h - (d * 4) + 14; },
+      'font-size': 12,
+      'fill': '#ffffff'
     });
 
 
-var svgLine = d3.select('#d3-line-chart')
-                  .append('svg')
-                  .attr({width: w, height: h});
+  var svgLine = d3.select('#d3-line-chart')
+    .append('svg')
+    .attrs({width: w, height: h});
 
-var monthlySales = [
+  var monthlySales = [
     {'month': 10, 'sales': 20},
     {'month': 20, 'sales': 14},
     {'month': 30, 'sales': 20},
@@ -47,17 +67,19 @@ var monthlySales = [
     {'month': 80, 'sales': 6},
     {'month': 90, 'sales': 23},
     {'month': 100, 'sales': 70}
-];
+  ];
 
-var lineFun = d3.svg.line()
+  var lineFun = d3.line()
     .x(function (d) { return d.month * 2; })
     .y(function (d) { return h - d.sales; })
-    .interpolate('linear');
+    .curve(d3.curveBasis);
 
-var viz = svgLine.append('path')
-        .attr({
-            d: lineFun(monthlySales),
-            'stroke': 'purple',
-            'stroke-width': 2,
-            'fill': 'none'
-        });
+  var viz = svgLine.append('path')
+    .attrs({
+      d: lineFun(monthlySales),
+      'stroke': 'purple',
+      'stroke-width': 2,
+      'fill': 'none'
+    });
+
+}());
